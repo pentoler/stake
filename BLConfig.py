f0dada028a075f0e5ea0db1567c43aa52db59eef
pentoler <77750693+pentoler@users.noreply.github.com>
2021-01-21 02:44:35 +0700

    Add files via upload

---
+++ BLConfig.py
@@ -0,0 +1,62 @@
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#      BL`378_project - Limbo v1.1.0 (87)
+#•••••••••••••••••••••••••••••••••••••••••••••••
+
+Token   = "8d19e8175189d742751d3d97daf9fcf307efd98ed2391cce275b029c7cf9f212f117dd908d00188753a5b272d0f641e1"
+
+Betting ={
+        "Base Bet"             : 0.00000001,
+
+        "Target Multiplier"    :{
+                                "Min": 14.1429,
+                                "Max": 14.1429
+                                },
+
+        "Max Base Bet"         : 0,
+
+        "Increase If Win"      : 1,
+        "Increase If Lose"     : 1.125,
+        "Reset If Profit"      : 0.0001,
+        "Reset If Lose"        : 0,
+        "Reset If Win Streak"  : 1,
+        "Reset If Lose Streak" : 0,
+
+        "Target Balance"       : 500000
+        }
+
+Extra   ={
+        "If Profit"            : 37.8,
+        "If Lose"              : 0,
+        
+        "Action"               : "Sleep",  # Sleep or ReLogin
+        "Delay"                : 9
+        }
+
+Bonus   ={
+        "Status"                          : "off",
+        "If Lose Streak"                  : 5,
+        "Repeat Bets"                     : 10,
+        "Increase Bet Amount After Repeat": 0
+        
+        # After max Number Repeats is reached,
+        # Bet will continue with increasing bet amount
+        # using Increase Bet Amount After Repeat
+        }
+
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#
+#  Best Regards:
+#
+#  • Kapal Api Special MIX
+#  • semua admin #Taraje_
+#  • semua admin Bahu Jalan
+#  • @prasun_parate untuk fungsi Fibonacci
+#  • ~RcX untuk beberapa fungsi yang saya gunakan
+#  • Siapapun Anda dg Dukungannya
+#  • and All Source Providers
+#  •••••
+#
+#  #bancetz.Laut_
+#  @ballataxart  (tg)
+#  378bancetz.laut@gmail.com
+#•••••••••••••••••••••••••••••••••••••••••••••••
